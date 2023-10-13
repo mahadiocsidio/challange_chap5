679380188009d1f4eaed468471ac0917070668d3
mahadiocsidio <mahadiocsidio@students.amikom.ac.id>
2023-10-13 23:42:30 +0700

    almost finish

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { prismaErrorHandler, errorHandler, notFound } = require('./middlewares/prisma.error');
 const { PORT = 3000 } = process.env;
 const app = express();
 
@@ -8,6 +9,9 @@ app.use(express.json());
 app.use('/api/v1', require('./routes/routing'));
 app.get('/api', (req, res) => res.send('OK'));
 
+app.use(prismaErrorHandler);
+app.use(notFound);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
